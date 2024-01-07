9194a53cf3dcbfc352493a2c92b7834ac9bf31f1
_BEK <62075760+BEK23@users.noreply.github.com>
2024-01-07 22:14:44 +0500

    add: react-router-dom

---
+++ src/layout/GuardLayout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function GuardLayout({ children }: { children: ReactNode }) {
+  return children;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import GuardLayout from "./layout/GuardLayout.tsx";
+import HomePage from "./pages/Home.tsx";
+import AboutPage from "./pages/About.tsx";
+import ContactsPage from "./pages/Contacts.tsx";
+import Layout from "./layout/Layout.tsx";
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <GuardLayout>
+        <Layout />
+      </GuardLayout>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/contacts",
+        element: <ContactsPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
